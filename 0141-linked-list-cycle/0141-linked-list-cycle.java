201e5ab761b6bac50d315301b0f891eb45784292
Govind Heda <111783844+govindheda070504@users.noreply.github.com>
2026-07-17 23:44:21 +0530

    Time: 0 ms (100%), Space: 47 MB (15.93%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -1,4 +1,3 @@
-import java.util.*;
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -10,7 +9,6 @@ import java.util.*;
  *     }
  * }
  */
-
  //  we can have 2 solution  1. Using Hashset  add the each element to the hashset and and checking wheather it is already present in the hashset or not !
  // 2. using using slow and fast pointer //Algorithm: Floyd's Tortoise and Hare
 
@@ -18,19 +16,25 @@ import java.util.*;
 // fast moves 2 steps at a time.
 // If there is a cycle, fast will eventually catch up with slow.
 // If fast reaches null (or fast.next == null), there is no cycle.
+
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet <ListNode> result = new HashSet<>();
-        while(head!=null){
-            if(result.contains(head)){
+     ListNode slow = head;
+     ListNode fast = head;
+     // Empty list or single node without a self-loop
+    if (head==null||head.next == null){
+        return false;
+    }
+        // Move slow by 1 step and fast by 2 steps
+     while (fast != null && fast.next!=null){ // we will run the loop until fast pointer  becomes null if  in between  fast and slow pointer becomes equal means cycle exists
+            fast = fast.next.next;
+            slow = slow.next;
+         // If they meet, a cycle exists
+            if(fast == slow){
                 return true;
-            }
-            else{
-                result.add(head);
-            }
-            head=head.next;
+            }            
         }
         return false;
-        
     }
 }
